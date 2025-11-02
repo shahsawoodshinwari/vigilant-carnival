20022a41e843146291572cdd56c4d942c4d18d9c
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-11-02 17:52:46 +0500

    Deploy: Build output

--- assets/status-tap-DjyeTcA7.js
+++
@@ -1,3 +0,0 @@
-import{ab as r,ac as i,ad as c,ae as d,af as l}from"./index-BpJpZ3ht.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
