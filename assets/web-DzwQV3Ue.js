148ff3f20c3170778b97faef862b3e873b58d630
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-11-03 03:46:39 +0500

    Deploy: Build output

--- assets/web-DzwQV3Ue.js
+++
@@ -1,1 +0,0 @@
-import{aJ as a}from"./index-BrJRRs5X.js";class e extends a{async canShare(){return"undefined"!=typeof navigator&&navigator.share?{value:!0}:{value:!1}}async share(a){if("undefined"==typeof navigator||!navigator.share)throw this.unavailable("Share API not available in this browser");return await navigator.share({title:a.title,text:a.text,url:a.url}),{}}}export{e as ShareWeb};
