5659cb8dfa2b5cb8a3bc871532d12b60518d4216
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-11-06 04:06:36 +0500

    Deploy: Build output

--- assets/web-C4XyAGAc.js
+++
@@ -1,1 +0,0 @@
-import{aG as e}from"./index-CtpMeeL6.js";class n extends e{async canShare(){return typeof navigator>"u"||!navigator.share?{value:!1}:{value:!0}}async share(a){if(typeof navigator>"u"||!navigator.share)throw this.unavailable("Share API not available in this browser");return await navigator.share({title:a.title,text:a.text,url:a.url}),{}}}export{n as ShareWeb};
