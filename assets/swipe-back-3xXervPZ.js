e0d0630d3e0fcfc8582322286c17d6bfd5449b63
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-11-08 23:54:03 +0500

    Deploy: Build output

--- assets/swipe-back-3xXervPZ.js
+++
@@ -1,3 +0,0 @@
-import{at as h,au as D,av as M}from"./index-CNp2Jb8-.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const k=(n,m,g,p,X)=>{const c=n.ownerDocument.defaultView;let s=h(n);const v=t=>{const{startX:e}=t;return s?e>=c.innerWidth-50:e<=50},i=t=>s?-t.deltaX:t.deltaX,w=t=>s?-t.velocityX:t.velocityX;return D({el:n,gestureName:"goback-swipe",gesturePriority:101,threshold:10,canStart:t=>(s=h(n),v(t)&&m()),onStart:g,onMove:t=>{const e=i(t)/c.innerWidth;p(e)},onEnd:t=>{const o=i(t),e=c.innerWidth,r=o/e,a=w(t),y=e/2,l=a>=0&&(a>.2||o>y),u=(l?1-r:r)*e;let d=0;if(u>5){const f=u/Math.abs(a);d=Math.min(f,540)}X(l,r<=0?.01:M(0,r,.9999),d)}})};export{k as createSwipeBackGesture};
