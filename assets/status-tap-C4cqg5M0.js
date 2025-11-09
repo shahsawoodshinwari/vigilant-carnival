36d7db306b73ae5dfe70cbef9323e6959eaeb7df
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-11-09 05:55:43 +0500

    Deploy: Build output

--- assets/status-tap-C4cqg5M0.js
+++
@@ -1,3 +0,0 @@
-import{az as r,aA as i,aB as c,aC as d,aD as l}from"./index-BySPAs2-.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
--- assets/swipe-back-BYLTCCGj.js
+++
@@ -1,3 +0,0 @@
-import{au as h,av as D,aw as M}from"./index-BySPAs2-.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const k=(n,m,g,p,w)=>{const c=n.ownerDocument.defaultView;let s=h(n);const X=t=>{const{startX:e}=t;return s?e>=c.innerWidth-50:e<=50},i=t=>s?-t.deltaX:t.deltaX,v=t=>s?-t.velocityX:t.velocityX;return D({el:n,gestureName:"goback-swipe",gesturePriority:101,threshold:10,canStart:t=>(s=h(n),X(t)&&m()),onStart:g,onMove:t=>{const e=i(t)/c.innerWidth;p(e)},onEnd:t=>{const o=i(t),e=c.innerWidth,r=o/e,a=v(t),y=e/2,l=a>=0&&(a>.2||o>y),u=(l?1-r:r)*e;let d=0;if(u>5){const f=u/Math.abs(a);d=Math.min(f,540)}w(l,r<=0?.01:M(0,r,.9999),d)}})};export{k as createSwipeBackGesture};
