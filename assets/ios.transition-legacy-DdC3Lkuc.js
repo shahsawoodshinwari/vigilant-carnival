fbeeff0b3798c0f8df7f5de8f64f54a23ab72c89
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-11-13 03:09:23 +0500

    Deploy: Build output

--- assets/ios.transition-legacy-DdC3Lkuc.js
+++ assets/ios.transition-legacy-DdC3Lkuc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CFSTRFZm.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.c,n=t.g}],execute:function(){
+System.register(["./index-legacy-B1pml83p.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.c,n=t.g}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DLVqxGGf.js
+++ assets/swipe-back-legacy-DLVqxGGf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CFSTRFZm.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-B1pml83p.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
